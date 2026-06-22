e9f5f6672c3e201773a598ca051d509102a0bc23
Chiranjit Majumdar <gameonzone5-lab@github.com>
2026-06-22 15:37:45 +0000

    Annihilated Termux from all bootstrap error popups

---
+++ nuclear_wipe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "১. সব ভাষার XML ফাইল থেকে Termux এর নামগন্ধ মুছে ফেলা হচ্ছে..."
+find . -type f -name "strings.xml" -exec sed -i 's/>Termux</>AutoKaaj</g' {} +
+find . -type f -name "strings.xml" -exec sed -i 's/>termux</>AutoKaaj</g' {} +
+
+echo "২. আপনার স্ক্রিনশটের ওই নির্দিষ্ট এরর মেসেজগুলো মুছে AutoKaaj বসানো হচ্ছে..."
+# Termux was unable -> AutoKaaj was unable
+find . -type f -name "strings.xml" -exec sed -i 's/Termux was unable/AutoKaaj was unable/g' {} +
+# unable to install boostrap -> AutoKaaj Engine
+find . -type f -name "strings.xml" -exec sed -i 's/unable to install bootstrap/unable to install AutoKaaj Engine/g' {} +
+
+echo "গড মোড ফিল্টার কমপ্লিট! আর কোথাও Termux নেই।"
